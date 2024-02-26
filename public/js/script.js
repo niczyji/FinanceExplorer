ea752e8b9d30cb54d75eab2e981f076ebff90af6
niczyji <niczyji@gmail.com>
2024-02-26 12:20:26 +0100

    Erweiterte Finanzinformationen und Formatierung in UI hinzugefügt

--- public/js/script.js
+++ public/js/script.js
@@ -93,17 +93,220 @@ document.addEventListener("DOMContentLoaded", function () {
             `;
             break;
           case "cryptocurrency":
+            content = `
+                  <li class="metadata-item">
+                    <div><strong>Name:</strong> ${
+                      src.name || "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Typ:</strong> ${
+                      src.type || "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Preis:</strong> ${
+                      src.price
+                        ? formatCurrency(src.price, src.currency) +
+                          " " +
+                          src.currency
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Marktkapitalisierung:</strong> ${
+                      src.marketCapitalization && src.marketCapitalization.value
+                        ? formatMarketCap(src.marketCapitalization.value) +
+                          " " +
+                          src.currency
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Volumen:</strong> ${
+                      src.volume
+                        ? src.volume.toLocaleString("de-DE") + " Einheiten"
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Webseite:</strong> <a href="${
+                      src.webUrl
+                    }" target="_blank">${src.webUrl}</a></div>
+                  </li>
+              `;
+            break;
           case "exchange traded commodity":
             content = `
-            <li class="metadata-item">
-            <div><strong>Name:</strong> ${src.name || "Nicht verfügbar"}</div>
-            <div><strong>Typ:</strong> ${src.type || "Nicht verfügbar"}</div>
-          </li>
-            `;
+                <li class="exchange-item">
+                  <div><strong>Name:</strong> ${
+                    src.name || "Nicht verfügbar"
+                  }</div>
+                  <div><strong>Typ:</strong> ${
+                    src.type || "Nicht verfügbar"
+                  }</div>
+                  <div><strong>ISIN:</strong> ${
+                    src.isin || "Nicht verfügbar"
+                  }</div>
+                  <div><strong>Total Assets:</strong> ${
+                    src.exchangeTradedFundDetails?.totalAssets.toLocaleString(
+                      "de-DE"
+                    ) || "Nicht verfügbar"
+                  } EUR</div>
+                  <div><strong>52-Wochen-Spanne:</strong> ${formatCurrency(
+                    src.technicals?.["52WeekLow"]
+                  )} - ${formatCurrency(src.technicals?.["52WeekHigh"])}</div>
+                  <div><strong>1Y Volatility:</strong> ${
+                    src.exchangeTradedFundDetails?.performance[
+                      "1YVolatility"
+                    ] || "Nicht verfügbar"
+                  }%</div>
+                  <div><strong>3Y Volatility:</strong> ${
+                    src.exchangeTradedFundDetails?.performance[
+                      "3YVolatility"
+                    ] || "Nicht verfügbar"
+                  }%</div>
+                  <div><strong>3Y Expected Return:</strong> ${
+                    src.exchangeTradedFundDetails?.performance["3YExpReturn"] ||
+                    "Nicht verfügbar"
+                  }%</div>
+                  <div><strong>3Y Sharpe Ratio:</strong> ${
+                    src.exchangeTradedFundDetails?.performance[
+                      "3YSharpRatio"
+                    ] || "Nicht verfügbar"
+                  }</div>
+                  <div><strong>Returns YTD:</strong> ${
+                    src.exchangeTradedFundDetails?.performance["returnsYtd"] ||
+                    "Nicht verfügbar"
+                  }%</div>
+                  <div><strong>Returns 1Y:</strong> ${
+                    src.exchangeTradedFundDetails?.performance["returns1Y"] ||
+                    "Nicht verfügbar"
+                  }%</div>
+                  <div><strong>Returns 3Y:</strong> ${
+                    src.exchangeTradedFundDetails?.performance["returns3Y"] ||
+                    "Nicht verfügbar"
+                  }%</div>
+                  <div><strong>Returns 5Y:</strong> ${
+                    src.exchangeTradedFundDetails?.performance["returns5Y"] ||
+                    "Nicht verfügbar"
+                  }%</div>
+                  <div><strong>Returns 10Y:</strong> ${
+                    src.exchangeTradedFundDetails?.performance["returns10Y"] ||
+                    "Nicht verfügbar"
+                  }%</div>
+                </li>
+              `;
             break;
           case "fund":
+            content = `
+                <li class="metadata-item">
+                  <div><strong>Name:</strong> ${
+                    src.name || "Nicht verfügbar"
+                  }</div>
+                  <div><strong>Typ:</strong> ${
+                    src.type || "Nicht verfügbar"
+                  }</div>
+                  <div><strong>ISIN:</strong> ${
+                    src.isin || "Nicht verfügbar"
+                  }</div>
+                  <div><strong>Marktkapitalisierung:</strong> ${
+                    src.marketCap
+                      ? formatMarketCap(src.marketCap)
+                      : "Nicht verfügbar"
+                  }</div>
+                  <div><strong>Handelsvolumen:</strong> ${
+                    src.volume
+                      ? src.volume.toLocaleString("de-DE") + " Stück"
+                      : "Nicht verfügbar"
+                  }</div>
+                  <div><strong>Preis:</strong> ${
+                    src.price
+                      ? src.price.toFixed(2) + " EUR"
+                      : "Nicht verfügbar"
+                  }</div>
+                  <div><strong>52-Wochen-Spanne:</strong> ${
+                    src.week52Range ? src.week52Range : "Nicht verfügbar"
+                  }</div>
+                </li>
+              `;
+            break;
           case "index":
+            // Das ISIN-Feld ist vorhanden, WKN-Feld fehlt im bereitgestellten Beispiel
+            const indexComponentsCount = src.indexComponents
+              ? src.indexComponents.length
+              : "Nicht verfügbar";
+            content = `
+                  <li class="metadata-item">
+                    <div><strong>Name:</strong> ${
+                      src.name || "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Typ:</strong> ${
+                      src.type || "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Anzahl der Indexkomponenten:</strong> ${indexComponentsCount}</div>
+                  </li>
+                `;
+            break;
           case "mutual fund":
+            content = `
+                  <li class="metadata-item">
+                    <div><strong>Name:</strong> ${
+                      src.name || "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Typ:</strong> ${
+                      src.type || "Nicht verfügbar"
+                    }</div>
+                    <div><strong>ISIN:</strong> ${
+                      src.isin || "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Marktkapitalisierung:</strong> ${
+                      src.marketCapitalization && src.marketCapitalization.value
+                        ? formatMarketCap(src.marketCapitalization.value)
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Handelsvolumen:</strong> ${
+                      src.volume
+                        ? src.volume.toLocaleString("de-DE") + " Stück"
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Preis:</strong> ${
+                      src.price ? formatCurrency(src.price) : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>52-Wochen-Spanne:</strong> ${
+                      src.week52Range || "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Ertragsrate:</strong> ${
+                      src.mutualFundDetails.yield
+                        ? (src.mutualFundDetails.yield * 100).toFixed(2) + "%"
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Ertragsrate YTD:</strong> ${
+                      src.mutualFundDetails.yieldYtd
+                        ? (src.mutualFundDetails.yieldYtd * 100).toFixed(2) +
+                          "%"
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Ertragsrate 1 Jahr YTD:</strong> ${
+                      src.mutualFundDetails.yield1YearYtd
+                        ? src.mutualFundDetails.yield1YearYtd.toFixed(2) + "%"
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Ertragsrate 3 Jahre YTD:</strong> ${
+                      src.mutualFundDetails.yield3YearYtd
+                        ? src.mutualFundDetails.yield3YearYtd.toFixed(2) + "%"
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Ertragsrate 5 Jahre YTD:</strong> ${
+                      src.mutualFundDetails.yield5YearYtd
+                        ? src.mutualFundDetails.yield5YearYtd.toFixed(2) + "%"
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>NAV:</strong> ${
+                      src.mutualFundDetails.nav
+                        ? formatCurrency(parseFloat(src.mutualFundDetails.nav))
+                        : "Nicht verfügbar"
+                    }</div>
+                    <div><strong>Vorheriger Schlusskurs:</strong> ${
+                      src.mutualFundDetails.prevClosePrice
+                        ? formatCurrency(
+                            parseFloat(src.mutualFundDetails.prevClosePrice)
+                          )
+                        : "Nicht verfügbar"
+                    }</div>
+                  </li>
+              `;
+            break;
           case "exchange traded fund":
             if (src.exchangeTradedFundDetails) {
               const etfDetails = src.exchangeTradedFundDetails;
@@ -181,9 +384,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  function formatCurrency(value) {
+  function formatCurrency(value, currency = "EUR") {
     if (typeof value === "number") {
-      return value.toFixed(2) + " EUR";
+      return new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: currency,
+      }).format(value);
     }
     return "Nicht verfügbar";
   }
